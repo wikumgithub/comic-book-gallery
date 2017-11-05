e790852e4d72f1665bffecc4ec666c4958410b4f
wikum <wikum@xcendant.com>
2017-11-05 12:30:53 +0530

    Add model classes for CommicBook and Artist

---
+++ src/CommicBookGallery/Models/CommicBook.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommicBookGallery.Models
+{
+    public class CommicBook
+    {
+        public int Id { get; set; }        
+        public string SeriesTitle{ get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public  Artist[] artists{ get; set; }
+        public bool Favourite { get; set; }     
+
+
+
+    }
+}
